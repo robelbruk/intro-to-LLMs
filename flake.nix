2835e29a328bc3c6a3aaa225f35e74567a7f410c
robelbruk <robelbruk5@gmail.com>
2025-04-03 21:01:52 -0400

    feat: notebook for OpenAI playground

--- flake.nix
+++ flake.nix
@@ -1,33 +1,32 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
-    };
-    outputs = { self, nixpkgs, flake-utils, ... }:
-        flake-utils.lib.eachDefaultSystem (system: 
-          let 
-            pkgs = import nixpkgs { inherit system; };
-            envWithScript = pkgs.buildFHSUserEnv {
-                name = "python-env";
-                targetPkgs = pkgs: with pkgs; [
-                    python3
-                    python3Packages.pip
-                    python3Packages.virtualenv
-                    pythonManylinuxPackages.manylinux2014Package
-                    cmake
-                    ninja
-                    gcc
-                ];
-                runScript = "${pkgs.writeShellScriptBin "runScript" ''
-                  set -e
-                  test -d .nix-venv || ${pkgs.python3.interpreter} -m venv .nix-venv
-                  source .nix-venv/bin/activate
-                  echo "You are now inside the FHS Python env. Install packages with pip."
-                  exec bash
-                ''}/bin/runScript";
-            };
-        in {
-            devShell = envWithScript;
-        } 
-    ); 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python3
+            python3Packages.pip
+            python3Packages.virtualenv
+            python3Packages.setuptools
+            python3Packages.wheel
+          ];
+
+          shell = "${pkgs.zsh}/bin/zsh";
+
+          shellHook = ''
+            export PYTHONNOUSERSITE="true"
+            test -d .nix-venv || python3 -m venv .nix-venv
+            source .nix-venv/bin/activate
+            echo ".nix-venv activated ✅. pip install packages like numpy, jupyter, etc."
+          '';
+        };
+      }
+    );
 }
